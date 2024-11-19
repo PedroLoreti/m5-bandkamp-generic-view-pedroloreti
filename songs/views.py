cdce0a8783b85763bd71bb1b2d2ad87045b1eb78
Loreti <pedroloreti639@gmail.com>
2024-11-18 23:57:28 -0300

    feat: tarefa x finalizada

--- songs/views.py
+++ songs/views.py
@@ -1,36 +1,23 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView, Response, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Song
-from rest_framework.pagination import PageNumberPagination
 from .serializers import SongSerializer
 from albums.models import Album
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.generics import ListCreateAPIView
 
 
-class SongView(APIView, PageNumberPagination):
+class SongView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def get(self, request, pk):
-        """
-        Obtençao de musicas
-        """
-        songs = Song.objects.filter(album_id=pk)
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
 
-        result_page = self.paginate_queryset(songs, request)
-        serializer = SongSerializer(result_page, many=True)
+    def perform_create(self, serializer):
+        found_album = get_object_or_404(Album, pk=self.kwargs["pk"])
+        return serializer.save(album=found_album)
 
-        return self.get_paginated_response(serializer.data)
-
-    def post(self, request, pk):
-        """
-        Criaçao de musica
-        """
-        album = get_object_or_404(Album, pk=pk)
-
-        serializer = SongSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(album=album)
-
-        return Response(serializer.data, status.HTTP_201_CREATED)
+    def get_queryset(self):
+        album = get_object_or_404(Album, pk=self.kwargs["pk"])
+        return self.queryset.filter(album=album)
